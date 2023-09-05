07ea223a77bb08e59e5c3374d4d5350fec2bb6c8
Imran Abdullah <nagatoyahiko856@gmail.com>
2023-09-05 08:38:49 +0300

    Initial Commit

---
+++ simple-chat/simple_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_chatApp.swift
+//  simple-chat
+//
+//  Created by Imran Abdullah on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct simple_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
